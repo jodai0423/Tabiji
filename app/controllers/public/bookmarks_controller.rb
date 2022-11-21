7a44cdc9ab8c4a0b3d3dd714476d53fb5f13a5b7
jodai <105637942+jodai0423@users.noreply.github.com>
2022-11-21 12:30:35 +0000

    update

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -2,14 +2,14 @@ class Public::BookmarksController < ApplicationController
 
   def create
     @bookmarked_post = Bookmark.new(user_id: current_user.id, post_id: params[:post_id])
+    @post = @bookmarked_post.post
     @bookmarked_post.save
-    redirect_to post_path(params[:post_id])
   end
 
   def destroy
     @bookmarked_post = Bookmark.find_by(user_id: current_user.id, post_id: params[:post_id])
+    @post = @bookmarked_post.post
     @bookmarked_post.destroy
-    redirect_to post_path(params[:post_id])
   end
 
   def index
